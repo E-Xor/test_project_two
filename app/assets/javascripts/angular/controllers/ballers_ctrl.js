46e00c98c12e69b7cd5bc6f59202ae00ca43004d
Maksim Sundukov <e-xormail@tut.by>
2014-01-11 17:32:20 -0500

    Table with filter and sort

--- app/assets/javascripts/angular/controllers/ballers_ctrl.js
+++ app/assets/javascripts/angular/controllers/ballers_ctrl.js
@@ -1,9 +1,15 @@
-// NgApp.controller('BallersCtrl', ['$scope', function($scope) {
-
-//   }
-// ]);
-
 NgApp.controller('BallersCtrl', ['$scope', 'Ballers', function($scope, Ballers) {
+
+    $scope.changeSortIcon = function (id, reverse) {
+      $('#sortIndicator').remove();
+      var indicator = 'sort_up.png';
+      if(reverse) {
+        indicator = 'sort_down.png';
+      }
+
+      $('#' + id).append("<img id='sortIndicator' src='/assets/"+indicator+"'/>");
+    }
+
     return $scope.players = Ballers.query();
   }
 ]);
