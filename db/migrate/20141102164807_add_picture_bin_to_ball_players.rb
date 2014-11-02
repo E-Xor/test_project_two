635de67479ccc20582e20932faa4bac86d102434
E-Xor <e-xormail@tut.by>
2014-11-02 12:40:04 -0500

    Fix PostgreSQL migration

--- db/migrate/20141102164807_add_picture_bin_to_ball_players.rb
+++ db/migrate/20141102164807_add_picture_bin_to_ball_players.rb
@@ -1,5 +1,9 @@
 class AddPictureBinToBallPlayers < ActiveRecord::Migration
   def change
-    add_column :ball_players, :picture_bin, :text, after: :picture, limit: 4294967295
+    if ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      add_column :ball_players, :picture_bin, :text, after: :picture
+    else
+      add_column :ball_players, :picture_bin, :text, after: :picture, limit: 4294967295
+    end
   end
 end
